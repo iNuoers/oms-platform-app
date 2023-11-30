2d82ea6827639ceea7954ac04bee00eb5eb05795
Ben <550461173@qq.com>
2023-11-30 21:56:57 +0800

    feat: vue&ts同时兼具 - eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: Ben 550461173@qq.com
  * @Date: 2023-11-30 21:31:24
  * @LastEditors: Ben 550461173@qq.com
- * @LastEditTime: 2023-11-30 21:48:27
+ * @LastEditTime: 2023-11-30 21:56:16
  * @FilePath: \oms-platform-app\.eslintrc.js
  * @Description: ESLint 检查 .vue 文件需要单独配置编辑器 ==> https://eslint.vuejs.org/user-guide/#editor-integrations
  */
@@ -12,51 +12,48 @@ module.exports = {
     node: true,
     browser: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  overrides: [
-    {
-      files: ["src/**/*.ts"],
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
-        extraFileExtensions: [".vue"],
-      },
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"],
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      excludedFiles: "*.test.ts",
-    },
+  extends: [
+    // lint vue 文件
+    // eslint-config-taro配置包中没有引入依赖eslint-plugin-vue
+    // 所以需要项目安装eslint-plugin-vue
+    'taro/vue4',
+    // eslint 集成prettier 规则，
+    // 需要先安装
+    // eslint-config-prettier
+    // eslint-plugin-prettier
+    // 1. disabled掉eslint中和prettier想冲突的规则
+    // 2. 实时以eslint 提示的方式显示prettier规则
+    // @see https://github.com/prettier/eslint-plugin-prettier
+    'plugin:prettier/recommended',
   ],
-  rules: {
-    '@typescript-eslint/unbound-method': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-function': 'warn',
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        extendDefaults: true,
-        types: {
-          '{}': false,
-        },
-      },
-    ],
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-debugger': 'warn',
-    'no-case-declarations': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-  },
+  rules: {},
+  // 方式一：
+  // lint js?(x) ts?(x)文件
+  // eslint-config-taro 没有引入相关依赖
+  // 需要项目单独安装：
+  // "eslint-plugin-import": "^2.26.0",
+  // "eslint-plugin-react": "^7.30.0",
+  // "eslint-plugin-react-hooks": "^4.5.0",
+  // glob functionally: https://github.com/isaacs/node-glob
+  overrides: [{ files: ['**/*.js?(x)', '**/*.ts?(x)'], extends: ['taro'] }],
+  // 方式二：
+  // 不使用taro配置包，自定义lint ts文件
+  // overrides: [
+  //   {
+  //     files: ["src/**/*.ts"],
+  //     parserOptions: {
+  //       tsconfigRootDir: __dirname,
+  //       project: ["./tsconfig.json"],
+  //       extraFileExtensions: [".vue"],
+  //     },
+  //     parser: "@typescript-eslint/parser",
+  //     plugins: ["@typescript-eslint"],
+  //     extends: [
+  //       "eslint:recommended",
+  //       "plugin:@typescript-eslint/recommended",
+  //       "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  //     ],
+  //     excludedFiles: "*.test.ts",
+  //   },
+  // ],
 }
